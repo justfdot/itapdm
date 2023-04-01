24668f6c36e2fd6f25a2222b457e3ab3a639325e
Ivan Filatov <justfdot@gmail.com>
2023-04-01 07:26:21 +0500

    Create init.cmd

---
+++ init.cmd
@@ -0,0 +1,8 @@
+# Политика исполнения для области "текущий пользователь"
+Set-ExecutionPolices
+
+# Путь до директории с изображениями
+$destination = '//remote/path'
+# $destination = '$env:LOCALAPPDATA\itapdm'
+
+# Задать 
